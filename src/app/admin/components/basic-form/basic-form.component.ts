8fae2d2623a6310b44a576c3fd88736ad10ec325
pjyeah <pespinoza9494@utm.edu.ec>
2023-10-27 11:08:38 -0500

    Capitulo 07

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -7,7 +7,12 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-  nameField = new FormControl('soy un control');
+  nameField = new FormControl('');
+  emailField = new FormControl('');
+  phoneField = new FormControl('');
+  colorField = new FormControl('#000000');
+  dateField = new FormControl('');
+  ageField = new FormControl('');
 
   constructor() {}
 
