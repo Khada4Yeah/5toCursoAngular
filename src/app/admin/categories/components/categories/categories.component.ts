4abaa5fb4a11bf23a52590ce714a70c7b608cad7
pjyeah <pespinoza9494@utm.edu.ec>
2023-11-17 22:39:27 -0500

    Capitulo 20 Arreglado todo

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from 'src/app/core/models/product.model';
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[] = [];
+  displayedColumns: string[] = ['id', 'name', 'actions'];
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  private getCategories() {
+    this.categoriesService.getAllCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+  }
 }
+import { CategoriesService } from 'src/app/core/services/categories.service';
--- src/app/core/models/product.model.ts
+++ src/app/core/models/product.model.ts
@@ -1,7 +1,21 @@
-export interface Product {
-  _id: string;
+export interface Category {
+  id: number;
   name: string;
-  price: number;
-  description: string;
   image: string;
 }
+
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  images: string[];
+  description: string;
+  category: Category;
+  taxes?: number;
+}
+
+export interface CreateProductDTO extends Omit<Product, 'id' | 'category'> {
+  categoryId: number;
+}
+
+export interface UpdateProductDTO extends Partial<CreateProductDTO> {}
